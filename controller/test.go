afcaf1d5f09b0d1728a009cd7bc9fe5bc12b8165
ahjiat <ah_jiat@hotmail.com>
2021-11-09 10:51:31 +0000

    changes

---
+++ controller/test.go
@@ -0,0 +1,29 @@
+package controller
+import (
+	. "github.com/ahjiat/gomvclib/basecontroller"
+	_"encoding/json"
+	_"html/template"
+)
+
+type Test struct{ BaseController }
+func (self Test) Index() {
+	/*
+	js := struct {
+		Title string	`json:"title"`
+		Age int			`json:"age"`
+	} { "welcome", 40 };
+	html := struct {
+		Name string		`json:"name"`
+		Num	int			`json:"num"`
+	} { "power", 20 }
+	self.ViewBag = struct {
+		Js interface{}
+		Html interface{}
+	} {
+		js,
+		html,
+	}
+	*/
+	self.ViewBag = "hello"
+	self.View()
+}
